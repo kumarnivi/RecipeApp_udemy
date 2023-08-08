479dfb0080a929fc956810877c076ec2ef70d274
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-08 23:35:10 +0530

    add Recipelist and items

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { Recipe } from './recipe.model';
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.css']
 })
 export class RecipeComponent {
-
+ selectedRecipe: Recipe ;
 }
