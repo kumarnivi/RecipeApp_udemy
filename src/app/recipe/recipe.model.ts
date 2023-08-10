12232b2e77634f3eff32a3621aa997ad937c4893
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-10 21:29:25 +0530

    add List of Ingeridents

--- src/app/recipe/recipe.model.ts
+++ src/app/recipe/recipe.model.ts
@@ -1,11 +1,15 @@
+import { Ingrediant } from "../shared/ingrediant.model";
+
 export class Recipe {
     public name:string;
     public desc:string;
     public imgPath:string;
+    public ingrediants:Ingrediant[];
 
-    constructor(name:string, desc:string, imgPath:string) {
+    constructor(name:string, desc:string, imgPath:string, ingrediants: Ingrediant[]) {
         this.name =name;
         this.desc = desc;
         this.imgPath = imgPath;
+        this.ingrediants = ingrediants;
     }
 }
--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,10 +1,18 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingrediant } from "../shared/ingrediant.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private  recipes:Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply and teast','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe('A Test Recipe', 
+        'This is simply and teast',
+        'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+        [
+          new Ingrediant('Meat', 1),
+          new Ingrediant('bun', 2)
+        ]
+        )
     
       ];
       getRecipes(){
