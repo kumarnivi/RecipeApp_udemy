a75d71df3c07dcb9301f15e5be1f582f3a5f284c
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-11 20:53:21 +0530

    Chield Routting Together

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeComponent } from './recipe/recipe.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full'},
   { path: 'recipes', component: RecipeComponent, children: [
-    {path : ''}
+    {path : '', component:RecipeStartComponent}
   ]},
   { path: 'shopping-list', component: ShoppingListComponent},
 
