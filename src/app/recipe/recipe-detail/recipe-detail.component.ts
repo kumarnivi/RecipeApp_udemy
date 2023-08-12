a8103f48adc8c4604b2bbd56c895fc7992a2db06
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-12 23:29:49 +0530

    navigate to the edit page

--- src/app/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
@@ -13,7 +13,9 @@ export class RecipeDetailComponent implements OnInit {
 // ** inject the recipe service here **//
 
   constructor ( private recipeService: RecipeService,
-                private route : ActivatedRoute ){
+                private route : ActivatedRoute,
+                private router:Router 
+                ){
     
   }
   ngOnInit() {
@@ -28,5 +30,10 @@ export class RecipeDetailComponent implements OnInit {
 
   onAddToShoppingList(){
   this.recipeService.addIngrediantsToShoppingList(this.recipe.ingrediants)
+  }
+
+  onEditRecipe() {
+   this.router.navigate(['edit'], {relativeTo:this.route})
+  // this.router.navigate(['../', this.id, 'edit'], {relativeTo:this.route})
   }
 };
