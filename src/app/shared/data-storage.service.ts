f1d27b5b2ae33aa6fff79aee4274e5de9e9e26b4
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-17 20:48:04 +0530

    save Recipe With FireBase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from "@angular/core"
 import { HttpClient } from "@angular/common/http"
+import { RecipeService } from "../recipe/recipe.service"
 
 @Injectable({providedIn: 'root'}) //** optional */
-export class DssataStorageService  {
- constructor (private http : HttpClient) {}
-    
+export class DataStorageService  {
+ constructor (private http : HttpClient, private recipeService: RecipeService) {}
+
+ storeRecipe() {
+  const recipes = this.recipeService.getRecipes()
+ return this.http.put('https://recipe-6a877-default-rtdb.firebaseio.com/recipes.json', recipes)
+ .subscribe(response => {
+    console.log(response)
+ })
+ }
 }
