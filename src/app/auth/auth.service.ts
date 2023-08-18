e4b14074527a21ceb47d2c60a761f3a74096ec11
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-18 12:09:42 +0530

    Login Error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 
@@ -19,25 +19,44 @@ export class AuthService {
   return  this.http.post<AuthResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAyWd3npD6Xnm3gQx_hVStgU2NbbGBAX5s',
       { email: email, password: password, returnSecureToken: true }
-    ).pipe(catchError( errorRes => {
-      let ErrorMesage = 'An Unknwon Error'
-      if (!errorRes.error || !errorRes.error.error) {
-        return throwError(ErrorMesage)
-      }
-      switch (errorRes.error.error.message) {
-        case 'EMAIL_EXISTS':
-        ErrorMesage = 'This Email alredy exits '
-      }
-      return throwError(ErrorMesage);
-    }));
+    )
+    .pipe(
+      catchError( this.handleError ));
   }
 
    // ** for login
 
    login( email:string,password:string) {
    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAyWd3npD6Xnm3gQx_hVStgU2NbbGBAX5s',
-    {email: email, password: password, returnSecureToken: true}
-    );
+    {
+      email: email, 
+      password: password, 
+      returnSecureToken: true
+    }
+    )
+    .pipe(
+      catchError( this.handleError ));
 
+  }
+
+  //** error Handling */
+  private handleError(errorRes:HttpErrorResponse) {
+    let ErrorMesage = 'An Unknwon Error'
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(ErrorMesage)
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+      ErrorMesage = 'This Email alredy exits ';
+      break;
+      case 'EMAIL_NOT_FOUND' :
+      ErrorMesage = 'This email dose not exists';
+      break;
+      case 'INVALID_PASSWORD' :
+        ErrorMesage = 'Password is invalid!'
+      break;
+    }
+    return throwError(ErrorMesage);
+  
   }
 }
