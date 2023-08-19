a7c39820b409aac5e12045961a3c9f00c38630c7
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-19 13:32:10 +0530

    fixed Bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,22 +9,31 @@ import { RecipesResolverService } from './recipe/recipes.resolver.service';
 import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  { path: 'recipes', component: RecipeComponent, children: [
-    {path : '', component:RecipeStartComponent},
-    { path: 'new', component:RecipeEditComponent},
-    {path: ':id', component:RecipeDetailComponent, resolve: [RecipesResolverService]},   
-    { path: ':id/edit', component:RecipeEditComponent,resolve: [RecipesResolverService]},
-
-  ]},
-  { path: 'shopping-list', component: ShoppingListComponent},
-  { path: 'auth', component:AuthComponent},
-
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  {
+    path: 'recipes',
+    component: RecipeComponent,
+    children: [
+      { path: '', component: RecipeStartComponent },
+      { path: 'new', component: RecipeEditComponent },
+      {
+        path: ':id',
+        component: RecipeDetailComponent,
+        resolve: [RecipesResolverService],
+      },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent,
+        resolve: [RecipesResolverService],
+      },
+    ],
+  },
+  { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'auth', component: AuthComponent },
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
