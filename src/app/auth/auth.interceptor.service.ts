3d894d6cbcebdfabb9c62af606006b14e02d50af
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-21 19:51:37 +0530

    component Optimition

--- src/app/auth/auth.interceptor.service.ts
+++ src/app/auth/auth.interceptor.service.ts
@@ -1,3 +1,34 @@
+// import { Injectable } from '@angular/core';
+// import {
+//   HttpInterceptor,
+//   HttpRequest,
+//   HttpHandler,
+//   HttpParams
+// } from '@angular/common/http';
+// import { take, exhaustMap } from 'rxjs/operators';
+
+// import { AuthService } from './auth.service';
+
+// @Injectable()
+// export class AuthInterceptorService implements HttpInterceptor {
+//   constructor(private authService: AuthService) {}
+
+//   intercept(req: HttpRequest<any>, next: HttpHandler) {
+//     return this.authService.user.pipe(
+//       take(1),
+//       exhaustMap(user => {
+//         if (!user) {
+//           return next.handle(req);
+//         }
+//         const modifiedReq = req.clone({
+//           params: new HttpParams().set('auth', user.token)
+//         });
+//         return next.handle(modifiedReq);
+//       })
+//     );
+//   }
+// }
+
 import { Injectable } from '@angular/core';
 import {
   HttpInterceptor,
@@ -21,10 +52,11 @@ export class AuthInterceptorService implements HttpInterceptor {
           return next.handle(req);
         }
         const modifiedReq = req.clone({
-          params: new HttpParams().set('auth', user.token)
+          // params: new HttpParams().set('auth', user.token)
         });
         return next.handle(modifiedReq);
       })
     );
   }
 }
+
